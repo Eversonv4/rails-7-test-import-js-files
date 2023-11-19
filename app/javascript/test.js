9f489c18a2b81bbff726f54de64343c411df35a0
Everson Soares <eversonv4@gmail.com>
2023-11-19 19:49:40 -0300

    uploading to github

---
+++ app/javascript/test.js
@@ -0,0 +1,4 @@
+alert(`
+será que funciona arquivo js no mesmo nivel do application.js?
+R: Funciona, mas como o arquivo está no mesmo nível, no momento de importar, precisa passar o ponto e a barra "./nome_do_arquivo"
+`);
